6c32f3180860a832ccb332ef2cec04e84686b1c2
Ilja-ops <kendamaprost@gmail.com>
2026-04-15 12:37:17 +0300

    Update Test_Ilja.py
    переделал код для того что бы можно было добавлять новые поля для ввода и что бы менять их расстояние  между собой

--- Test_Ilja.py
+++ Test_Ilja.py
@@ -6,10 +6,12 @@ class MyWindow:
   self.window.title("Andmete sisestamine")
   self.window.geometry("600x500")
 
-#name
-self.label_name = tk.Label(self.window, text="Имя")
-        self.label_name.pack()
+# Заголовки
+        tk.Label(self.window, text="Имя").grid(row=0, column=0)
+        tk.Label(self.window, text="Фамилия").grid(row=0, column=1, padx=15)
+        
+        # Поля ввода (под ними)
+        self.entry_name = tk.Entry(self.window)
+        self.entry_name.grid(row=1, column=0)
 
-#поле ввода
-self.entry_name = tk.Entry(self.window)
-self.entry_name.pack()
+        
