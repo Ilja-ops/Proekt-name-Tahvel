0967437132fe34f5664f11d7b0871cd64e062a40
Ilja-ops <kendamaprost@gmail.com>
2026-04-15 11:21:05 +0300

    Update Test_Ilja.py
    дописал окно и добавил поле ввода для имени

--- Test_Ilja.py
+++ Test_Ilja.py
@@ -4,4 +4,12 @@ class MyWindow:
   def __init__(self): 
   self.window = tk.Tk()
   self.window.title("Andmete sisestamine")
-  
+  self.window.geometry("600x500")
+
+#name
+self.label_name = tk.Label(self.window, text="Имя")
+        self.label_name.pack()
+
+#поле ввода
+self.entry_name = tk.Entry(self.window)
+self.entry_name.pack()
