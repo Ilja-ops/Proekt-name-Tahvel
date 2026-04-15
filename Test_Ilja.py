58f46fa917a6216d1f40f8b93caa37866b81df91
Ilja-ops <kendamaprost@gmail.com>
2026-04-15 12:51:16 +0300

    Update Test_Ilja.py
    добавил поле ввода для личного кода и дописал код окошка что бы оно открывалось

--- Test_Ilja.py
+++ Test_Ilja.py
@@ -2,16 +2,29 @@ import tkinter as tk
 
 class MyWindow:
   def __init__(self): 
-  self.window = tk.Tk()
-  self.window.title("Andmete sisestamine")
-  self.window.geometry("600x500")
+     self.window = tk.Tk()
+     self.window.title("Andmete sisestamine")
+     self.window.geometry("600x500")
 
 # Заголовки
-        tk.Label(self.window, text="Имя").grid(row=0, column=0)
-        tk.Label(self.window, text="Фамилия").grid(row=0, column=1, padx=15)
+     tk.Label(self.window, text="Имя").grid(row=0, column=0)
+     tk.Label(self.window, text="Фамилия").grid(row=0, column=1, padx=15)
         
         # Поля ввода (под ними)
-        self.entry_name = tk.Entry(self.window)
-        self.entry_name.grid(row=1, column=0)
+     self.entry_name = tk.Entry(self.window)
+     self.entry_name.grid(row=1, column=0)
 
-        
+     self.entry_surname = tk.Entry(self.window)
+     self.entry_surname.grid(row=1, column=1, padx=15)
+
+     tk.Label(self.window, text="Личный код").grid(row=2, column=0, columnspan=2)
+
+        #поле ввода для личного кода
+     self.entry_code = tk.Entry(self.window)
+     self.entry_code.grid(row=3, column=0, columnspan=2, padx=15, pady=5)
+
+  def run(self):
+    self.window.mainloop()
+
+app = MyWindow()
+app.run()
