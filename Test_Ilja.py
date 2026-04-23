7320cc833b7f31bf04cc7ba3b9b82c87ae31bd07
Ilja-ops <kendamaprost@gmail.com>
2026-04-23 13:19:19 +0300

    Update Test_Ilja.py
    сделал кнопку зарегистрироватся, начинаю думать как сделать сохранение 
    
    Tegin registreerumisnupu, hakkan mõtlema, kuidas salvestamise funktsiooni teha

--- Test_Ilja.py
+++ Test_Ilja.py
@@ -8,53 +8,53 @@ class MyWindow:
     self.window = ctk.CTk()
     self.window.title("Tahvel")
     self.window.geometry("500x600")
-#коробка которая не даст поля ввода быть разбросанными 
-#kast, mis hoiab sisestusväljad korras 
+# коробка которая не даст поля ввода быть разбросанными 
+# kast, mis hoiab sisestusväljad korras 
     
 # Заголовок
 # Pealkirja
     ctk.CTkLabel(frame, text="Registreerimine", font=("Arial", 20)).pack(pady=10)
-    # Поля ввода
-    # Sisestusväljad 
+# Поля ввода
+# Sisestusväljad 
 
-    #поле под имя(внутри поля)
-    #nimeväli (välja sees)
+# поле под имя(внутри поля)
+# nimeväli (välja sees)
     self.entry_name = ctk.CTkEntry(frame, placeholder_text="Nimi")
     self.entry_name.pack(pady=5, fill="x")
-#поле под фамлию(внутри поля)
-#perekonnanime väli (välja sees)
+# поле под фамлию(внутри поля)
+# perekonnanime väli (välja sees)
     self.entry_surname = ctk.CTkEntry(frame, placeholder_text="perekonnanimi")
     self.entry_surname.pack(pady=5, fill="x")
-#поле под личный код(внутри поля)
-#isikliku koodi väli (välja sees)
+# поле под личный код(внутри поля)
+# isikliku koodi väli (välja sees)
     self.entry_code = ctk.CTkEntry(frame, placeholder_text="isikukood")
     self.entry_code.pack(pady=5, fill="x")
-#поле под телефон(внутри поля)
-#telefoninumber (välja sees)
+# поле под телефон(внутри поля)
+# telefoninumber (välja sees)
     self.entry_phone = ctk.CTkEntry(frame, placeholder_text="Telefon")
     self.entry_phone.pack(pady=5, fill="x")
-#поле под дату рождения(внутри поля)
-#sünnikuupäeva väli (välja sees)
+# поле под дату рождения(внутри поля)
+# sünnikuupäeva väli (välja sees)
     self.entry_age = ctk.CTkEntry(frame, placeholder_text="Sünnikuupäev")
     self.entry_age.pack(pady=5, fill="x")
-#поле под год окончания школы(внутри поля)
-#kooli lõpetamise aasta väli (välja sees)
+# поле под год окончания школы(внутри поля)
+# kooli lõpetamise aasta väli (välja sees)
     self.entry_school = ctk.CTkEntry(frame, placeholder_text="Kooli lõpetamise aasta")
     self.entry_school.pack(pady=5, fill="x")
-#поле под пол(внутри поля)
-#väli sugu all (välja sees)  
+# поле под пол(внутри поля)
+# väli sugu all (välja sees)  
     self.entry_sugu = ctk.CTkEntry(frame, placeholder_text="sugu")
     self.entry_sugu.pack(pady=5, fill="x")
-#поле под адрес(внутри поля)
-#aadressiväli (välja sees)
+# поле под адрес(внутри поля)
+# aadressiväli (välja sees)
     self.entry_sugu = ctk.CTkEntry(frame, placeholder_text="Aadress")
     self.entry_sugu.pack(pady=5, fill="x")
 
-  # кнопка сохранить
-  #salvestamise nupp
-    tk.Button(self.window, text="Сохранить", command=self.save).grid(row=6, column=0, columnspan=2)
-#здесь мы пишем то что будет сохранять
-#siin kirjutame selle, mis salvestatakse
+# кнопка сохранить
+# salvestamise nupp
+    Ctk.CTkButton(frame, text="Registreeruda", command=self.save).pack(padx=20)
+# здесь мы пишем то что будет сохранять
+# siin kirjutame selle, mis salvestatakse
 def save(self):
       name = self.entry_name.get()
       surname = self.entry_surname.get()
